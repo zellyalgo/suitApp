9f09733ec79a7819fc86a291a5c0c193d198d6f1
ZellyAlgo <zell.y.algo@gmail.com>
2016-01-28 21:39:31 +0100

    refactor imageController

---
+++ SuitApp/app/src/main/java/com/zellyalgo/suitapp/controllers/ImageControl.java
@@ -0,0 +1,52 @@
+package com.zellyalgo.suitapp.controllers;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * Created by zellyalgo on 28/1/16.
+ */
+public class ImageControl {
+
+    public static ImageControl createImageController (){
+
+    }
+
+    public Bitmap ShrinkBitmap(String file, int width, int height){
+
+        BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+        bmpFactoryOptions.inJustDecodeBounds = true;
+
+        int heightRatio = (int)Math.ceil(bmpFactoryOptions.outHeight/(float)height);
+        int widthRatio = (int)Math.ceil(bmpFactoryOptions.outWidth / (float) width);
+
+        if (heightRatio > 1 || widthRatio > 1)
+        {
+            if (heightRatio > widthRatio)
+            {
+                bmpFactoryOptions.inSampleSize = heightRatio;
+            } else {
+                bmpFactoryOptions.inSampleSize = widthRatio;
+            }
+        }
+
+        bmpFactoryOptions.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeFile(file, bmpFactoryOptions);
+        return getResizedBitmap(bitmap, width);
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 0) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+}
